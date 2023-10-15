82a399b37a7dc9eed8abffe968f3dee3d7f73527
good-mistake <devillmk88@gmail.com>
2023-10-15 15:32:12 +0330

    adding remove

--- todo.js
+++ todo.js
@@ -1,35 +1,74 @@
 const inp = document.getElementById("input");
 const btn = document.getElementById("btn");
 const container = document.querySelector(".container");
+const ul = document.querySelector("ul");
+let myList = [];
 
-const myList = [];
 const createElements = (val) => {
-  const ul = document.querySelector("ul");
+  const removeBtn = document.createElement("button");
   const item = document.createElement("li");
+  removeBtn.textContent = "Remove";
+
+  val > "" ? ul.appendChild(item) : [];
   item.textContent = val;
-  container.appendChild(ul);
-  ul.appendChild(item);
+  item.append(removeBtn);
+  for (let i = 0; i < myList.length; i++) {
+    const listItem = myList[i];
+    if (listItem.title === val) {
+      removeBtn.addEventListener("click", () => {
+        const index = myList.indexOf(listItem);
+        item.remove();
+        myList.splice(index, 1);
+        save();
+      });
+    }
+  }
 };
-const save = () => {
-  myList.push(inp.value);
+function clearInput() {
+  inp.value = "";
+}
+const syncfunc = () => {
+  const allLists = JSON.parse(localStorage.getItem("List")) || [];
+  myList = allLists;
+  for (let i = 0; i < allLists.length; i++) {
+    const all = allLists[i].title;
+    createElements(all);
+  }
+};
+const save = (item) => {
+  if (item) {
+    const nextitem = {
+      title: item.title,
+    };
+    myList.push(nextitem);
+  }
+
   const list = JSON.stringify(myList);
   localStorage.setItem("List", list);
 };
-btn.addEventListener("click", () => {
-  console.log(inp.value);
-  if (inp.value === "") {
-    inp.classList.add("red");
-  } else {
-    save();
-    createElements(inp.value);
-    inp.value = "";
-    inp.classList.remove("red");
-    console.log(myList);
-  }
-});
+function setupTodo() {
+  btn.addEventListener("click", () => {
+    const val = input.value;
+    if (val === "") {
+      inp.classList.add("red");
+    } else {
+      createElements(val);
+      save({
+        title: val,
+      });
 
-const allLists = JSON.parse(localStorage.getItem("List"));
-for (let i = 0; i < allLists.length; i++) {
-  const all = allLists[i];
-  createElements(all);
+      clearInput();
+      inp.classList.remove("red");
+      console.log(myList);
+    }
+  });
 }
+
+function init() {
+  syncfunc();
+  createElements();
+  setupTodo();
+  console.log(myList);
+}
+
+init();
